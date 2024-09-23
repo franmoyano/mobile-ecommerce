4025b772a65e40632da248a665152a372c0e39ad
Franco Moyano <koprammoyano@gmail.com>
2024-09-23 10:10:58 -0300

    - Se agrega detalle de orden

--- src/screens/OrderDetail.js
+++ src/screens/OrderDetail.js
@@ -1,7 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useGetOrderByUserQuery } from "../services/orders";
+import NoContent from "./NoContent";
+import { colors } from "../global/colors";
+import { toCurrency } from "../utils/string-utils";
 
 const OrderDetail = ({ route }) => {
   const { id } = route.params;
@@ -15,13 +18,55 @@ const OrderDetail = ({ route }) => {
     if (isSuccess) console.log(order);
   }, [isSuccess]);
 
+  if (!order) {
+    return <NoContent />;
+  }
+
   return (
     <View>
-      <Text>OrderDetail</Text>
+      <Text style={styles.date}>Fecha: {order.createdAt}</Text>
+      <Text style={styles.total}>Total: {toCurrency(order.total)}</Text>
+      <FlatList
+        data={order.items}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.price}>{toCurrency(item.price)}</Text>
+            <Text style={styles.price}>Cantidad: {item.quantity}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
 
 export default OrderDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  date: {
+    fontSize: 16,
+    textAlign: "center",
+  },
+  listItem: {
+    backgroundColor: colors.blue1,
+    width: "90%",
+    margin: "auto",
+    borderRadius: 4,
+    marginTop: 4,
+    padding: 15,
+  },
+  title: {
+    fontSize: 20,
+    color: "#FFF",
+  },
+  price: {
+    color: "#FFF",
+  },
+  total: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 15,
+  },
+});
